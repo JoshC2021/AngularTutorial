1b1917245354865b90fd3d791618878d1aa3eac5
Josh <thejosh2021@gmailcom>
2021-05-24 10:13:48 -0400

    removed weather

--- TestingAngular/Controllers/WeatherForecastController.cs
+++ TestingAngular/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestingAngular.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Freezing"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
